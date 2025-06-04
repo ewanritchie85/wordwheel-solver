3d86829a9c71910a65beb9007e7e2208849f4779
EWAN <ewanritchie@hotmail.co.uk>
2025-06-04 18:44:09 +0100

    word_list.txt changed to hardcoded EOWL word list and logic for creating new list removed as EOWL is more accurate

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-from pprint import pprint
 
 from input import input_centre_letter, input_outer_letters
 from solver import solve, get_valid_word_count
-from utils import create_word_list
 
 
 def main():
@@ -10,7 +8,6 @@ def main():
     Main function to run the word game solver.
     Prompts the user for input and displays the results.
     """
-    create_word_list()
 
     centre_letter = input_centre_letter()
     outer_letters = input_outer_letters()
@@ -21,7 +18,7 @@ def main():
     print(f"Valid words found: {valid_word_count}")
     print("Words:")
 
-    pprint(valid_words)
+    print(valid_words)
 
 
 if __name__ == "__main__":
